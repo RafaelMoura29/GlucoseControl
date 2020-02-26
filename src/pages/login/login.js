552847bb46cc0d6765c538beb56ae6055fbeac26
Gabriel Moura <rafaelmoura2929@gmail.com>
2020-02-26 10:02:18 -0300

    Criando layout login

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,9 +1,22 @@
 import React, { Component } from "react";
+import "./login.css";
 
 export default class Login extends Component {
   render() {
     return (
-     <h1>Login</h1>
+        <div id="container">
+          <h1 id="title">BlackBook</h1>
+          <p>
+            <input type="text" className="text-input"></input>
+          </p>
+          <p>
+            <input type="text" className="text-input"></input>
+          </p>
+          <p>
+            <button type="button" href="#" className="login-button">Login</button>
+          </p>
+          <p href="#" id="register-text">Não tem cadastro ainda? <a href="google.com">cadastre-se</a></p>
+        </div>
     );
   }
 }
