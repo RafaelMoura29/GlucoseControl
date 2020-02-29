54f79a03142908a7de0fbd57859a636e93d51974
Gabriel Moura <rafaelmoura2929@gmail.com>
2020-02-29 11:52:43 -0300

    Removendo componentes

--- src/routes.js
+++ src/routes.js
@@ -16,50 +16,13 @@
 
 */
 import Dashboard from "views/Dashboard.jsx";
-import Map from "views/Map.jsx";
-import Notifications from "views/Notifications.jsx";
-import TableList from "views/TableList.jsx";
-import UserProfile from "views/UserProfile.jsx";
 
 var routes = [
   {
     path: "/dashboard",
     name: "Dashboard",
-    rtlName: "لوحة القيادة",
     icon: "tim-icons icon-chart-pie-36",
     component: Dashboard,
-    layout: "/admin"
-  },
-  {
-    path: "/map",
-    name: "Map",
-    rtlName: "خرائط",
-    icon: "tim-icons icon-pin",
-    component: Map,
-    layout: "/admin"
-  },
-  {
-    path: "/notifications",
-    name: "Notifications",
-    rtlName: "إخطارات",
-    icon: "tim-icons icon-bell-55",
-    component: Notifications,
-    layout: "/admin"
-  },
-  {
-    path: "/user-profile",
-    name: "User Profile",
-    rtlName: "ملف تعريفي للمستخدم",
-    icon: "tim-icons icon-single-02",
-    component: UserProfile,
-    layout: "/admin"
-  },
-  {
-    path: "/tables",
-    name: "Table List",
-    rtlName: "قائمة الجدول",
-    icon: "tim-icons icon-puzzle-10",
-    component: TableList,
     layout: "/admin"
   }
 ];
