bb66ae2f675f61cf44ca23d759b72ed757b6e7c0
Gabriel Moura <rafaelmoura2929@gmail.com>
2020-03-03 23:05:10 -0300

    Definindo layout da página informações leito

--- src/views/Infos_leito.jsx
+++ src/views/Infos_leito.jsx
@@ -23,25 +23,18 @@ import { Line, Bar } from "react-chartjs-2";
 
 // reactstrap components
 import {
-    Button,
-    ButtonGroup,
     Card,
-    CardHeader,
     CardBody,
     CardTitle,
-    DropdownToggle,
-    DropdownMenu,
-    DropdownItem,
-    UncontrolledDropdown,
-    Label,
-    FormGroup,
-    Input,
-    Table,
     Row,
     Col,
     CardText,
-    UncontrolledTooltip,
-
+    CardSubtitle,
+    CardLink,
+    Table,
+    FormGroup,
+    Label,
+    Input
 } from "reactstrap";
 
 // core components
@@ -53,12 +46,47 @@ import {
 } from "variables/charts.jsx";
 
 class Info_leito extends React.Component {
-    
+
     render() {
         return (
             <>
                 <div className="content">
-                    <h1>Info leitos</h1>
+                    <Row>
+                        <Col md="7">
+                            <Card>
+                                <CardBody>
+                                    <CardTitle>
+                                        <h2 className="title">Nome leito</h2>
+                                    </CardTitle>
+                                    <CardText>*Paciente internado no leito ou opção para adicionar paciente ao leito</CardText>
+                                    <CardText>*Gráfico com quantidade de internações no leito por mês</CardText>
+
+                                    <h3 className="title">Internações</h3>
+                                    <Table responsive striped>
+                                        <thead className="text-primary">
+                                            <tr>
+                                                <th className="text-left">Paciente</th>
+                                                <th className="text-left">Entrada</th>
+                                                <th className="text-left">Saída</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td className="text-left">Peter Parker</td>
+                                                <td className="text-left">10/10/2019 - 18:30</td>
+                                                <td className="text-left">12/10/2019 - 13:59</td>
+                                            </tr>
+                                            <tr>
+                                                <td className="text-left">Mary Jane</td>
+                                                <td className="text-left">10/10/2019 - 18:31</td>
+                                                <td className="text-left">10/12/2019 - 10:30</td>
+                                            </tr>
+                                        </tbody>
+                                    </Table>
+                                </CardBody>
+                            </Card>
+                        </Col>
+                    </Row>
                 </div>
             </>
         );
