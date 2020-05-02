3a42526a981ac31a31a608dd46143dadd94faec4
rafael moura <rafaelmoura2929@gmail.com>
2020-05-02 14:52:30 -0300

    Criando página ajuda

--- src/views/Ajuda.jsx
+++ src/views/Ajuda.jsx
@@ -1,25 +1,13 @@
-/*!
-
-=========================================================
-* Black Dashboard React v1.0.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/black-dashboard-react
-* Copyright 2019 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/black-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import React from "react";
+import img1 from "../assets/img/imgs_ajuda/img1.png"
+import img2 from "../assets/img/imgs_ajuda/img2.png"
+import img3 from "../assets/img/imgs_ajuda/img3.png"
+import img4 from "../assets/img/imgs_ajuda/img4.png"
 
 import {
     Card,
-    CardBody
+    CardBody,
+    Alert
 } from "reactstrap";
 
 class Ajuda extends React.Component {
@@ -36,7 +24,27 @@ class Ajuda extends React.Component {
                 <div className="content">
                     <Card >
                         <CardBody>
-                           <h1>Página ajuda</h1>
+                            <h1>AJUDA</h1>
+
+                            <p className="mb-3" style={{ fontSize: '1.2em' }}>Desenvolvemos o GLYCON para ser o mais simples e intuitivo o possível, mas caso tenha alguma dúvida em relação a sua utilização pode entrar em contato conosco ou seguir as orientações abaixo.</p>
+                            <p className="mb-3" style={{ fontSize: '1.2em' }}>Na tela principal (figura 1), são listados todos os pacientes já cadastrados, caso deseje consultar ou alterar seus dados basta clicar no ícone editar (botão azul contendo o ícone de um lápis), caso deseje inserir um novo paciente, basta clicar no botão NOVO e para fazer uma nova coleta de glicemia é só clicar no botão coleta (representado pelo ícone de uma gota de sangue e de cor vermelha).</p>
+                            <img src={img1} />
+                            <p className="mb-3" style={{ fontSize: '0.9em', color: 'gray', textAlign: 'center' }}>Figura 1 - Tela Principal / Lista de Pacientes</p>
+
+
+                            <p className="mb-3" style={{ fontSize: '1.2em' }}>A tela de cadastro e atualização (figura 2) dos dados do paciente são a mesma, a única diferença é que ao escolher novo paciente a tela é aberta com os campos para preenchimento em branco e ao escolher atualizar os campos já vem preenchidos com os dados atuais do paciente.</p>
+                            <img src={img2} />
+                            <p className="mb-3" style={{ fontSize: '0.9em', color: 'gray', textAlign: 'center' }}>Figura 2 – Cadastro e Atualização dos dados do Paciente</p>
+
+                            <p className="mb-3" style={{ fontSize: '1.2em' }}>Após preencher todos os dados ou fazer a atualização necessária e preencher o plano de coletas (que representa a frequência que as coletas de glicemias devem ser respeitadas), basta salvar, voltar a tela inicial, clicando na opção Pacientes no menu lateral para prosseguir.</p>
+                            <p className="mb-3" style={{ fontSize: '1.2em' }}>Ao clicar em coleta, é apresentada a tela de coletas do paciente (figura 3), ou seja, uma lista com as coletas já realizadas para aquele paciente, caso já existam. Logo abaixo aparecerá um gráfico com os níveis de glicemia relacionados a lista de coletas. É possível escolher o período que deseja gerar o gráfico, caso necessário.</p>
+                            <img src={img3} />
+                            <p className="mb-3" style={{ fontSize: '0.9em', color: 'gray', textAlign: 'center' }}>Figura 3 – Coletas do Paciente</p>
+
+                            <p className="mb-3" style={{ fontSize: '1.2em' }}>Para inserir uma nova coleta de glicemia basta clicar no botão COLETA e a tela de coleta de glicemias será apresentada (figura 4). Nesta tela, basta entrar com os dados sugeridos, principalmente o valor da glicemia e clicar em SALVAR GLICEMIA.</p>
+                            <img src={img4} />
+                            <p className="mb-3" style={{ fontSize: '0.9em', color: 'gray', textAlign: 'center' }}>Figura 4 – Tela de Coletas de Glicemia</p>
+
                         </CardBody>
                     </Card>
                 </div>
