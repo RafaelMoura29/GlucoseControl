4f734e3348aacb8714972d0f707f16c11fae5c4b
Rafael Moura <rafaelmoura2929@gmail.com>
2020-06-12 15:44:05 -0300

    Adding navbar to login page

--- src/views/login/index.jsx
+++ src/views/login/index.jsx
@@ -1,22 +1,45 @@
 import React from "react";
 import './style.css'
 
-import {} from "reactstrap";
+import { Link } from "react-router-dom";
+import { Row, Col, Button, Container } from "reactstrap";
+import imgLogo from "../../assets/img/GLYCON-bco.png"
 
 class Login extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-    };
+      screenWidth: window.screen.width,
+      screenHeigth: window.screen.height,
+    }
   }
 
   render() {
     return (
       <>
-        <div className="content">
-            login
-        </div>
+        <Row style={{ margin: 0, padding: '25px 10px 17px 25px' }}>
+          <Col md="6">
+          <img src={imgLogo} alt="" height="76" width="257" />
+
+          </Col>
+          <Col md="6" style={{textAlign: "right"}}>
+            <Link style={{ marginRight: 12, marginLeft: 12, color: '#ddd' }} to={'#'}>
+              Sobre
+            </Link>
+            <Link style={{ marginRight: 12, marginLeft: 12, color: '#ddd' }} to={'#'}>
+              Contato
+            </Link>
+            <Link style={{ marginRight: 12, marginLeft: 12, color: '#ddd' }} to={'#'}>
+              Ajuda
+            </Link>
+            <Link style={{ marginRight: 12, marginLeft: 12 }} to={'#'}>
+              <Button color="primary" style={{ background: '#a564e9', fontWeight: 'unset' }}>
+                Assinar
+              </Button>
+            </Link>
+          </Col>
+        </Row>
       </>
     );
   }
