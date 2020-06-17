73d5ec5a0e579f00f744574205fbe5e851d5b814
Rafael Moura <rafaelmoura2929@gmail.com>
2020-06-17 13:56:26 -0300

    developing recover password screen

--- src/views/authentication/components/recoverPassword.jsx
+++ src/views/authentication/components/recoverPassword.jsx
@@ -1,10 +1,40 @@
 import React from 'react'
 
+import { Button, Input, Label, Container } from "reactstrap";
+
+import { Link } from "react-router-dom";
+
 const RecoverPassword = () => (
     <>
-    <h1>
-        Recover password component
-    </h1>
+    <Container className="themed-container" fluid={true} >
+      <p style={{ color: '#ddd', textAlign: 'justify', fontSize: '1.2em' }}>
+        Utilize o e-mail cadastrado para recuperar a senha.
+      </p>
+    </Container>
+
+    <Container className="themed-container" fluid={true} style={{ marginTop: 35 }} >
+
+      <Label style={{ color: '#ddd', marginBottom: 25, fontSize: 22 }}>
+        RESGATAR SENHA
+      </Label>
+
+      <Input type="email" placeholder="E-mail" style={{ marginBottom: 25 }} />
+
+    </Container>
+
+    <Container className="themed-container" fluid={true} style={{ marginBottom: 25 }} >
+      <Button id="btn-login" color="primary">
+        ENVIAR EMAIL
+      </Button>
+    </Container>
+
+    <Container className="themed-container text-center" fluid={true} style={{ marginBottom: 10 }} >
+      <Link to="/authentication/register" className="secondary-link">Ainda não tenho uma conta</Link>
+    </Container>
+
+    <Container className="themed-container text-center" fluid={true}>
+      <Link to="/authentication/login" className="secondary-link">Fazer login</Link>
+    </Container>
     </>
 )
 
