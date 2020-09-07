c9bb43735bbd5760b9f695cacb3bd72b933eba56
Rafael Moura <rafaelmoura2929@gmail.com>
2020-09-07 12:22:33 -0300

    Removing form component from form aplicação

--- src/views/formAplicacao/index.jsx
+++ src/views/formAplicacao/index.jsx
@@ -3,7 +3,6 @@ import LoadingSpinner from '../../components/LoadingSpinner.js'
 import ModalMessager from '../../components/ModalMessager/modalMessager'
 import './style.css'
 import api from '../../variables/api'
-import FormularioAplicacao from './components/form'
 import FormFooter from './components/formFooter'
 
 import {
@@ -12,6 +11,10 @@ import {
   Row,
   Col,
   ModalHeader,
+  FormGroup,
+Label,
+Input,
+Form
 } from "reactstrap";
 
 class FormAplicacao extends React.Component {
@@ -187,11 +190,161 @@ class FormAplicacao extends React.Component {
                   </Col>
                 </Row>
 
-                <FormularioAplicacao
-                  {...this.state.form}
-                  opcoesDroga={opcoesDroga}
-                  handleChange={this.handleChange}
+
+                <Form>
+
+      <Row>
+
+        <Col className="pr-md-1" md="6">
+          <Row>
+            <Col className="pr-md-1" md="6">
+              <FormGroup>
+                <Label>PRONTUÁRIO</Label>
+                <Input
+                  placeholder="Prontuário"
+                  type="text"
+                  name="prontuario"
+                  value={this.state.form.prontuario}
+                  disabled
                 />
+              </FormGroup>
+            </Col>
+
+            <Col className="pr-md-1" md="6">
+              <FormGroup>
+                <Label>DATA/HORA INTERNAÇÃO</Label>
+                <Input
+                  placeholder="Data/Hora"
+                  type="text"
+                  name="dataHoraInternacao"
+                  value={this.state.form.dataHoraInternacao}
+                  disabled
+                />
+              </FormGroup>
+            </Col>
+
+            <Col className="pr-md-1" md="12">
+              <FormGroup>
+                <Label>PACIENTE</Label>
+                <Input
+                  placeholder="Paciente"
+                  type="text"
+                  name="paciente"
+                  value={this.state.form.paciente}
+                  disabled
+                />
+              </FormGroup>
+            </Col>
+
+            <Col className="pr-md-1" md="6">
+              <FormGroup>
+                <Label>DATA APLICAÇÃO</Label>
+                <Input
+                  type="date"
+                  placeholder="Data Aplicação"
+                  value={this.state.form.dataAplicacao}
+                  onChange={this.handleChange}
+                  name="dataAplicacao"
+                  invalid={!this.state.form.dataAplicacao}
+                />
+              </FormGroup>
+            </Col>
+
+            <Col className="pr-md-1" md="6">
+              <FormGroup>
+                <Label>HORA APLICAÇÃO</Label>
+                <Input
+                  placeholder="Hora Aplicação"
+                  type="time"
+                  name="horaAplicacao"
+                  value={this.state.form.horaAplicacao}
+                  onChange={this.handleChange}
+                  invalid={!this.state.form.horaAplicacao}
+                />
+              </FormGroup>
+            </Col>
+
+            <Col className="pr-md-1" md="12">
+              <FormGroup>
+                <Label>TIPO APLICAÇÃO</Label>
+                <Input
+                  type="select"
+                  name="tipoAplicacao"
+                  value={this.state.form.tipoAplicacao}
+                  onChange={this.handleChange}
+                >
+                  <option value="de resgate">De Resgate</option>
+                  <option value="de horario">De Horário</option>
+                </Input>
+              </FormGroup>
+            </Col>
+          </Row>
+        </Col>
+
+        <Col className="pr-md-1" md="6">
+          <Col className="pr-md-1" md="12">
+            <FormGroup>
+              <Label>VIA ADMINISTRAÇÃO</Label>
+              <Input
+                type="select"
+                name="viaAdministracao"
+                value={this.state.form.viaAdministracao}
+                onChange={this.handleChange}
+              >
+                <option value="intravenoso">Intravenoso</option>
+                <option value="sub cutaneo">Sub Cutâneo</option>
+                <option value="via oral">Via Oral</option>
+              </Input>
+            </FormGroup>
+          </Col>
+
+          <Col className="pr-md-1" md="12">
+            <FormGroup>
+              <Label>DROGA</Label>
+              <Input
+                type="select"
+                name="droga"
+                value={this.state.form.droga}
+                onChange={this.handleChange}
+                invalid={!this.state.form.droga}
+                >
+                {opcoesDroga.map((opcao, index) => (
+                  <option key={index} value={opcao}>{opcao}</option>
+                ))}
+              </Input>
+            </FormGroup>
+          </Col>
+
+          <Col className="pr-md-1" md="12">
+            <FormGroup>
+              <Label>POSOLOGIA</Label>
+              <Input
+                placeholder="Posologia"
+                type="number"
+                name="posologia"
+                value={this.state.form.posologia}
+                onChange={this.handleChange}
+                invalid={!this.state.form.posologia}
+              />
+            </FormGroup>
+          </Col>
+
+          <Col className="pr-md-1" md="12">
+            <FormGroup>
+              <Label>OBSERVAÇÕES</Label>
+              <Input
+                type="textarea"
+                name="observacoes"
+                placeholder="Observações"
+                value={this.state.form.observacoes}
+                onChange={this.handleChange}
+              />
+            </FormGroup>
+          </Col>
+        </Col>
+
+      </Row>
+    </Form>
 
               </CardBody>
 
